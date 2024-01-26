c53573cf9c90e9e32c3f5fa2d1c9102db841b2f7
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2024-01-26 10:38:47 +0100

    fix entry.sh

--- entry.sh
+++ entry.sh
@@ -11,7 +11,7 @@ sed -i -e "s/{{SECRET_KEY}}/${SECRET_KEY}/g" \
 	-e "s/{{MAIN_DATABASE_USER}}/${MAIN_DATABASE_USER}/g" \
 	-e "s/{{MAIN_DATABASE_PASSWD}}/${MAIN_DATABASE_PASSWD}/g" \
 	-e "s/{{MAIN_DATABASE_HOST}}/${MAIN_DATABASE_HOST}/g" \
-	-e "s/{{MAIN_DATABASE_PORT}}/${MAIN_DATABASE_PORT}/g" .env
+	-e "s/{{MAIN_DATABASE_PORT}}/${MAIN_DATABASE_PORT}/g" ".env"
 	
 python -m venv venv
 
