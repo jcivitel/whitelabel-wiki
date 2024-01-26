a02bf1e1959bf0c96910d56ecb9f182261654116
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2024-01-26 10:47:06 +0100

    update entry.sh

--- entry.sh
+++ entry.sh
@@ -2,17 +2,17 @@
 
 cp env_template .env
 
-sed -i -e "s/{{SECRET_KEY}}/${SECRET_KEY}/g" \
-	-e "s/{{DEBUG}}/${DEBUG}/g" \
-	-e "s/{{LANGUAGE_CODE}}/${LANGUAGE_CODE}/g" \
-	-e "s/{{TIME_ZONE}}/${TIME_ZONE}/g" \
-	-e "s/{{MAIN_DATABASE_ENGINE}}/${MAIN_DATABASE_ENGINE}/g" \
-	-e "s/{{MAIN_DATABASE_NAME}}/${MAIN_DATABASE_NAME}/g" \
-	-e "s/{{MAIN_DATABASE_USER}}/${MAIN_DATABASE_USER}/g" \
-	-e "s/{{MAIN_DATABASE_PASSWD}}/${MAIN_DATABASE_PASSWD}/g" \
-	-e "s/{{MAIN_DATABASE_HOST}}/${MAIN_DATABASE_HOST}/g" \
-	-e "s/{{MAIN_DATABASE_PORT}}/${MAIN_DATABASE_PORT}/g" ".env"
-	
+sed -i -e "s/{{SECRET_KEY}}/${SECRET_KEY}/g" ".env"
+sed -i -e "s/{{DEBUG}}/${DEBUG}/g" 
+sed -i -e "s/{{LANGUAGE_CODE}}/${LANGUAGE_CODE}/g" 
+sed -i -e "s/{{TIME_ZONE}}/${TIME_ZONE}/g" 
+sed -i -e "s/{{MAIN_DATABASE_ENGINE}}/${MAIN_DATABASE_ENGINE}/g" 
+sed -i -e "s/{{MAIN_DATABASE_NAME}}/${MAIN_DATABASE_NAME}/g" 
+sed -i -e "s/{{MAIN_DATABASE_USER}}/${MAIN_DATABASE_USER}/g" 
+sed -i -e "s/{{MAIN_DATABASE_PASSWD}}/${MAIN_DATABASE_PASSWD}/g" 
+sed -i -e "s/{{MAIN_DATABASE_HOST}}/${MAIN_DATABASE_HOST}/g" 
+sed -i -e "s/{{MAIN_DATABASE_PORT}}/${MAIN_DATABASE_PORT}/g" ".env"
+
 python -m venv venv
 
 python pip install --upgrade pip
@@ -20,5 +20,5 @@ python pip install --upgrade pip
 . venv/bin/activate
 
 pip install -r requirements.txt
-	
+
 python manage.py runserver --noreload 0.0.0.0:8000
