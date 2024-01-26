97075b909388ee2040db5ff0b60a35878c26f34b
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2024-01-26 11:04:42 +0100

    added input file

--- entry.sh
+++ entry.sh
@@ -3,14 +3,14 @@
 cp env_template .env
 
 sed -i -e "s/{{SECRET_KEY}}/${SECRET_KEY}/g" ".env"
-sed -i -e "s/{{DEBUG}}/${DEBUG}/g" 
-sed -i -e "s/{{LANGUAGE_CODE}}/${LANGUAGE_CODE}/g" 
-sed -i -e "s/{{TIME_ZONE}}/${TIME_ZONE}/g" 
-sed -i -e "s/{{MAIN_DATABASE_ENGINE}}/${MAIN_DATABASE_ENGINE}/g" 
-sed -i -e "s/{{MAIN_DATABASE_NAME}}/${MAIN_DATABASE_NAME}/g" 
-sed -i -e "s/{{MAIN_DATABASE_USER}}/${MAIN_DATABASE_USER}/g" 
-sed -i -e "s/{{MAIN_DATABASE_PASSWD}}/${MAIN_DATABASE_PASSWD}/g" 
-sed -i -e "s/{{MAIN_DATABASE_HOST}}/${MAIN_DATABASE_HOST}/g" 
+sed -i -e "s/{{DEBUG}}/${DEBUG}/g" ".env"
+sed -i -e "s/{{LANGUAGE_CODE}}/${LANGUAGE_CODE}/g" ".env"
+sed -i -e "s/{{TIME_ZONE}}/${TIME_ZONE}/g" ".env"
+sed -i -e "s/{{MAIN_DATABASE_ENGINE}}/${MAIN_DATABASE_ENGINE}/g" ".env"
+sed -i -e "s/{{MAIN_DATABASE_NAME}}/${MAIN_DATABASE_NAME}/g" ".env"
+sed -i -e "s/{{MAIN_DATABASE_USER}}/${MAIN_DATABASE_USER}/g" ".env"
+sed -i -e "s/{{MAIN_DATABASE_PASSWD}}/${MAIN_DATABASE_PASSWD}/g" ".env"
+sed -i -e "s/{{MAIN_DATABASE_HOST}}/${MAIN_DATABASE_HOST}/g" ".env"
 sed -i -e "s/{{MAIN_DATABASE_PORT}}/${MAIN_DATABASE_PORT}/g" ".env"
 
 python -m venv venv
